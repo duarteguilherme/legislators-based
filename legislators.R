9ac45e1d8139a8cfa9cc57281e5ab94c3e989485
duarteguilherme <gjardimduarte@gmail.com>
2015-10-21 11:36:50 -0200

    Corrected model

--- legislators.R
+++ legislators.R
@@ -41,9 +41,15 @@ data {
 parameters {
   real beta0[K];
   real beta1[K];
-  real x[L];
+  vector[L-2] z;
 }
 transformed parameters {
+  vector[L] x;
+  for (i in 1:40) x[i] <- z[i];
+  for (i in 42:65) x[i] <- z[i-1];
+  for (i in 67:100) x[i] <- z[i-2];
+  x[41] <- 1;
+  x[66] <- -1;
 }
 
 model {
@@ -51,18 +57,39 @@ for (i in 1:N){
     y[i] ~ bernoulli(Phi_approx(x[jj[i]]*beta1[kk[i]] - beta0[kk[i]]));
 }
 
+beta1 ~ normal(0, 1);
+beta0 ~ normal(0, 1);
+z ~ normal(0,1);  
 
-for (i in 1:40){ x[i] ~ normal(0,1); }
-  for (i in 42:65){ x[i] ~ normal(0,1); }
-  for (i in 67:100){ x[i] ~ normal(0,1); }
-  beta1 ~ normal(0, 2);
-  beta0 ~ normal(0, 2);
-  x[41] ~ normal(-1, .0001);
-  x[66] ~ normal(1, .0001);
-}
-generated quantities { // Generated quantities block. Not used presently.
 }
 '
 fit <- stan(model_code = stanstr, data=data, iter=12000, warmup=2000, thin=10, chains=3)
 
+/* for (i in 1:40){ x[i] ~ normal(0,1); }
+for (i in 42:65){ x[i] ~ normal(0,1); }
+for (i in 67:100){ x[i] ~ normal(0,1); }
+x[41] ~ normal(-1, .0001);
+x[66] ~ normal(1, .0001);
+*/ 
+  
+  
+  
+  
+macs <- read.csv('~/Downloads/macs.csv', stringsAsFactors=FALSE)
 
+
+
+
+library(dplyr)
+
+
+
+macs <- select(macs, -Numero_ADI)
+
+nrow(macs)
+ncol(macs)
+
+teste <- table(macs)
+
+
+macs <- as.vector(macs)
